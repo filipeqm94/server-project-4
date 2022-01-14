2dcaab3e5aee854f06afc2f297405afcc9d191b8
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-14 12:54:35 -0500

    📝 Add local psql database to database list

--- project_4/settings.py
+++ project_4/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = "project_4.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chatter',
+        'USER': 'chatteruser',
+        'PASSWORD': 'chatter',
+        'HOST': 'localhost'
     }
 }
 
