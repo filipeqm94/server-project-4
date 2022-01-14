a55be7a717fb6a16741b0cb05729701be7b22303
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-14 16:32:36 -0500

    📝 Add token-auth path to project url patterns

--- chatter_project/urls.py
+++ chatter_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token-auth/', obtain_jwt_token)
 ]
