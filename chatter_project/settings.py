887d25a8569873eec9543321ff9817cbf2fddbf7
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-14 14:01:48 -0500

    📝 add user_profile app to project 📝 remove chatter_server app

--- chatter_project/settings.py
+++ chatter_project/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Custom apps
+    "user_profile",
+
+    # Default apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "chatter_server",
 ]
 
 MIDDLEWARE = [
