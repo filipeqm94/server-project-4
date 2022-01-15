aba6098f13e8f767693679fab9933a01e2f60057
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-15 10:21:13 -0500

    📝 Add drf-sjwt and drf urls

--- chatter_project/urls.py
+++ chatter_project/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('token-auth/', obtain_jwt_token)
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
