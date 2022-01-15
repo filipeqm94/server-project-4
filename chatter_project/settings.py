203253b4e51d93ecf7694eda2476e40db7cc981a
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-15 11:57:23 -0500

    add allowed host

--- chatter_project/settings.py
+++ chatter_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ["MODE"] == "dev" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ["APP_NAME"],]
 
 
 # Application definition
