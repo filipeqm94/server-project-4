c18fdd718a3f932712be9eb90cd133e65b15d6a5
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-16 12:18:47 -0500

    📝 Remove auth classes for user creation and add logout and blacklist view

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
 
@@ -12,6 +13,7 @@ class ObtainTokenPairWithLanguageView(TokenObtainPairView):
 
 class CustomUserCreate(APIView):
     permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
 
     def post(self, request, format="json"):
         serializer = CustomUserSerializer(data=request.data)
@@ -28,3 +30,15 @@ class ObtainTokenPairWithLanguagesView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
+class LogoutAndBlacklistRefreshTokenForUserView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
