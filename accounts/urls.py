73c40599225d58361c55e35aa6f7ef9e39104956
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-16 12:19:39 -0500

    📝 Add logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import ObtainTokenPairWithLanguageView, CustomUserCreate
+from .views import (
+    CustomUserCreate,
+    ObtainTokenPairWithLanguageView,
+    LogoutAndBlacklistRefreshTokenForUserView,
+)
+
 
 urlpatterns = [
-    path('user/create/', CustomUserCreate.as_view(), name="create_user"),
-    path('token/obtain/', ObtainTokenPairWithLanguageView.as_view(), name='token_create'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path("user/create/", CustomUserCreate.as_view(), name="create_user"),
+    path(
+        "token/obtain/", ObtainTokenPairWithLanguageView.as_view(), name="token_create"
+    ),
+    path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "blacklist/",
+        LogoutAndBlacklistRefreshTokenForUserView.as_view(),
+        name="blacklist",
+    ),
 ]
