76417ed6239ae0e523b1ed870c8e6466912d8b02
v-wang <victorwang920@gmail.com>
2022-01-22 17:10:40 -0500

    fixed model dunder methods

--- chatter/models.py
+++ chatter/models.py
@@ -7,9 +7,15 @@ class ChatRoom(models.Model):
   user_two = models.ForeignKey(CustomUser, related_name='user_two', on_delete=models.CASCADE)
   display_name = models.CharField(max_length=50)
 
+  def __str__(self):
+    return self.display_name
+
 class ChatMessage(models.Model):
   chat = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
   sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
   message = models.TextField()
   timestamp = models.DateTimeField(default=now, editable=False)
+
+  def __str__(self):
+    return self.sender.username
   
