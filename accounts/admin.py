b30c782673e2f30fc6ceed3ecdc51fe2e02711d3
v-wang <victorwang920@gmail.com>
2022-01-22 16:51:12 -0500

    registered ChatRoom and ChatMessage models to Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import CustomUser
+from chatter.models import *
 
 # Register your models here.
 class CustomUserAdmin(admin.ModelAdmin):
     model = CustomUser
 
+class ChatRoomAdmin(admin.ModelAdmin):
+    model = ChatRoom
+
+class ChatMessageAdmin(admin.ModelAdmin):
+    model = ChatMessage
+
 # register custom user and admin to `admin/`
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(ChatRoom, ChatRoomAdmin)
+admin.site.register(ChatMessage, ChatMessageAdmin)
+
