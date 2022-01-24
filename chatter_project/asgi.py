a118da72d2bc3eea9a89bb03966c63bf80dde9f1
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-24 10:46:38 -0500

    gotta try

--- chatter_project/asgi.py
+++ chatter_project/asgi.py
@@ -9,20 +9,28 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 import django
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chatter.routing
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatter_project.settings")
 django.setup()
+application = get_default_application()
 
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(chatter.routing.websocket_urlpatterns)
-        ),
-    }
-)
+# import os
+# import django
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# import chatter.routing
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatter_project.settings")
+# django.setup()
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http": get_asgi_application(),
+#         "websocket": AuthMiddlewareStack(
+#             URLRouter(chatter.routing.websocket_urlpatterns)
+#         ),
+#     }
+# )
