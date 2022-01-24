8392c13fc914b3e29432f306367b0ac9eb2162c0
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-24 09:29:02 -0500

    tryin everything but the kitchen sink

--- chatter_project/settings.py
+++ chatter_project/settings.py
@@ -184,4 +184,12 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 CORS_ALLOWED_ORIGINS = os.environ["CORS_ALLOWED_LIST"].split(",")
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+if not DEBUG:
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_FILE_OVERWRITE = False
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    S3_URL = f"http://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    
+    django_heroku.settings(locals())
