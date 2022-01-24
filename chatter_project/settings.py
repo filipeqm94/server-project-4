7605cad4799d0d40d10c6b901adb68be1b4cf3b2
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-24 09:23:19 -0500

    wtf;

--- chatter_project/settings.py
+++ chatter_project/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
-    "django-heroku",
     # local
     "accounts",
     "chatter",
